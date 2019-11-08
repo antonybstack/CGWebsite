6f7b217b023540988cae841dd7a196790d6d0e95
antonybstack <ablyakhe@uncc.edu>
2019-11-08 15:43:43 -0500

    updated jquery UI and plugins

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
+// using 2 jquery Widget factory effects
 $(document).ready(function() {
   $("#t").hover(
-      function () {
-        $(this).find('ul>li').show(300);
-      },
-      function () {
-        $(this).find('ul>li').hide(600);
-      }
-    );
+    function() {
+      $(this).find('ul>li').show(300);
+    },
+    function() {
+      $(this).find('ul>li').hide(600);
+    }
+  );
 
 
-    $('#accordion').accordion({
-        active: false,
-        collapsible: true
+  // jQuery plugin
+  if ($(".slideshow").length) {
+    $('.slideshow').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      cssEase: 'linear',
+      adaptiveHeight: true,
+      autoplay: true,
+      autoplaySpeed: 1500
     });
+  }
+
+  // jQuery UI Widget
+  if ($("#accordion").length) {
+    console.log("bye");
+    $('#accordion').accordion({
+      active: false,
+      collapsible: true
+    });
+  }
 });
